615d0b415b516980fb033b752174e1a8b8cb7aaa
Anthony Pauriche <anthony.p@dejbox.fr>
2020-02-22 12:57:00 +0100

    Initial Commit

---
+++ Otter/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Otter
+//
+//  Created by Anthony Dejbox on 22/02/2020.
+//  Copyright © 2020 Anthony Pauriche. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
