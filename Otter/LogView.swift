bd96627667e88d1707f319b1f04f333c6b68fee7
Anthony Pauriche <anthony.p@dejbox.fr>
2020-02-23 14:51:15 +0100

    Put log in scrollview

--- Otter/LogView.swift
+++ Otter/LogView.swift
@@ -16,37 +16,36 @@ struct LogView: View {
     
     var body: some View {
         HStack {
-            if isOpen {
-                VStack {
-                    Text(log.title)
-                        .bold()
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.bottom, 10)
-                    TextField("", text: .constant(log.text))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding()
-                        .background(Color(red: 0.1, green: 0.1, blue: 0.15))
-                        .cornerRadius(4)
-                }
-            } else {
+            VStack {
                 Text(log.title)
                     .bold()
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
+                    .padding(.bottom, isOpen ? 10 : 0)
+                if isOpen {
+                    ScrollView {
+                        Text(log.text)
+                            .frame(maxWidth: .infinity, alignment: .topLeading)
+                    }
+                    .frame(maxHeight: 300)
+                    .padding()
+                    .background(Color(red: 0.1, green: 0.1, blue: 0.15))
+                    .cornerRadius(4)
+                }
             }
             Divider()
             Image("chevron")
                 .rotationEffect(.degrees(
                     isOpen ? 180 : 0
                 ))
-                .frame(maxHeight: .infinity, alignment: .topLeading)
+                .frame(maxHeight: .infinity, alignment: .top)
         }
-        .frame(maxWidth: .infinity)
         .padding()
         .background(Color(red: 0.1, green: 0.3, blue: 0.4))
+        .cornerRadius(4)
+        .padding(EdgeInsets(top: 4, leading: 4, bottom: 0, trailing: 4))
         .onTapGesture {
             self.isOpen = !self.isOpen
         }
-        .cornerRadius(4)
     }
 }
 
@@ -55,7 +54,7 @@ struct LogView_Previews: PreviewProvider {
         LogView(log:
             Log(
                 id: 0, 
-                text: "[ 200 ] - https://www.efgoisder.com/tokens"
+                text: "[ 200 ] - https://www.efgoisder.com/token"
             )
         )
     }
